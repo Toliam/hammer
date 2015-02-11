e44a00cc8330f5612ea941eaa34da857eb2e01c8
Toliam <tolik_u1@mail.ru>
2015-02-11 21:04:36 +0200

    advance

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var main = function () {
 "use strict";
 $(".comment-input button").on("click", function (event) {
-$(".comments").append("<p>Это новый комментарий</p>");
+var $new_comment;
+if ($(".comment-input input").val() !== "") {
+var $new_comment = $("<p>").text($(".comment-input input").val());
+$(".comments").append($new_comment);
+$(".comment-input input").val("");
+}
 });
-};
 $(document).ready(main);
